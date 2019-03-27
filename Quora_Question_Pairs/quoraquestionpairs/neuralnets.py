c31075a590f62c8438ecc68707994a50e2dae9e8
Hravan <krzysztof.krenc@gmail.com>
2019-03-27 12:46:09 +0100

    Added simple neural network with GRU layer.

---
+++ Quora_Question_Pairs/quoraquestionpairs/neuralnets.py
@@ -0,0 +1,22 @@
+import torch
+
+from torch import nn
+
+class RNNGRU(nn.Module):
+    '''A recurrent neural network with GRU unit.'''
+
+    def __init__(self, n_vocab, emb_dim, hidden_size):
+        super().__init__()
+        self.emb = nn.Embedding(n_vocab, emb_dim)
+        self.gru = nn.GRU(emb_dim, hidden_size)
+        self.out = nn.Linear(hidden_size, 1)
+
+    def forward(self, x1, x2):
+        x1_emb = self.emb(x1)
+        x2_emb = self.emb(x2)
+
+        concatenated = torch.cat([x1_emb, x2_emb])
+        _, x = self.gru(concatenated)
+
+        out = torch.sigmoid(self.out(x))
+        return out
